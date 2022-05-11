821efc3338169b762226279d2d634e53713ace35
hum02 <ehdgma02@gmail>
2022-05-11 15:05:07 +0900

    스프링import

---
+++ src/main/java/com/movieinfo/sharewatch/SharewatchApplication.java
@@ -0,0 +1,13 @@
+package com.movieinfo.sharewatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharewatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharewatchApplication.class, args);
+	}
+
+}
