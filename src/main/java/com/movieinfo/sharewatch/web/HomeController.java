9b5652018962b2336cf5b82a8086a56fcb3de07d
hum02 <ehdgma02@gmail>
2022-06-29 23:58:32 +0900

    로그인,post생성 form

--- src/main/java/com/movieinfo/sharewatch/web/HomeController.java
+++ src/main/java/com/movieinfo/sharewatch/web/HomeController.java
@@ -1,26 +1,39 @@
 package com.movieinfo.sharewatch.web;
 
+import com.movieinfo.sharewatch.config.auth.dto.SessionUser;
 import com.movieinfo.sharewatch.domain.movie.movieRepository;
+import com.movieinfo.sharewatch.web.dto.PostsSaveRequestDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class HomeController {
 
     @Autowired
-    private movieRepository movieReposit;
+    private movieRepository movieRepository;
+    @Autowired
+    private HttpSession httpSession;
+
+    @GetMapping("/posts/save")
+    public String createForm(Model model, PostsSaveRequestDto requestDto){
+        model.addAttribute("requestDto",requestDto);
+        return "post_save";
+    }
 
     @GetMapping("/")
-    public String index(){
-        return "index";
-    }
+    public String login(Model model){
 
-    @GetMapping("/post")
-    public String postFront(){
-        return "post_register";
-    }
+        SessionUser user =(SessionUser) httpSession.getAttribute("user");
 
-    @GetMapping("/login")
-    public String login(){return "login_form";}
+        if (user != null) {
+            model.addAttribute("username", user.getName());
+            model.addAttribute("image",user.getPicture());
+        }
+
+        return "login_form";}
 }
