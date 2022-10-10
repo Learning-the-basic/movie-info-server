800ab70e2ddbab23295f37283f130e3eab9d4964
h1013iris <h1013iris@gmail.com>
2022-10-10 14:17:38 +0900

    2022.10.10 - ReviewController 주석 정리가 안되서 다시 push

--- src/main/java/com/movieinfo/sharewatch/web/ReviewController.java
+++ src/main/java/com/movieinfo/sharewatch/web/ReviewController.java
@@ -8,7 +8,6 @@ import com.movieinfo.sharewatch.web.dto.review.ReviewUpdateResponse;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
